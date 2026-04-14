faa3bb572295ee7d77ad8e08048b91ae8ac647b0
Pedro Barreto Palheta de Oliveira <pedro.barreto@dinfor.uniceub>
2026-04-13 21:48:52 -0300

    aula do dia 13/04

---
+++ aluno/inserir.php
@@ -0,0 +1,29 @@
+<?php
+
+    include "../conexao.php";
+
+    $id = isset($_POST["id"]) ? $_POST["id"] : null;
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+
+    
+    if($id) {
+        $sql = "UPDATE aluno SET nome = :nome, idade = :idade, email = :email, telefone = :telefone WHERE id = :id";
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(':id', $id);
+    } else {
+        $sql = "INSERT INTO aluno (nome, idade, email, telefone) VALUES (:nome, :idade, :email, :telefone)";
+        $stmt = $conexao->prepare($sql);
+    }
+    
+    $stmt->bindParam(':nome', $nome);
+    $stmt->bindParam(':idade', $idade);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':telefone', $telefone);
+    $stmt->execute();
+    header("Location:index.php");
+    exit();
+
+?>
